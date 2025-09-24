d7e69e1f2c07fb7e389590d08ff6880b3aa53731
akwanrealtor <akwanrealtor@gmail.com>
2025-09-24 16:53:11 -0400

    Create property management landing experience

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
